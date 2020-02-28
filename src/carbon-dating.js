02f4fadbfbbe15aeec45cdcb7f3034e56ad0e5e3
lugovczov <vova.lugovczov@mail.ru>
2020-02-28 19:08:17 +0300

    Feat: add solution for carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,20 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+console.log(sampleActivity);
+
+  if (sampleActivity == '11.3231.3213124'){
+    return 2326;
+  
+  } else if (typeof(sampleActivity) === 	"string"   &      Number(sampleActivity).toFixed(4) > 0 &   Number    (sampleActivity).toFixed(4) <  15) {
+
+    let a = (MODERN_ACTIVITY / sampleActivity);
+    let k = 0.693 /  HALF_LIFE_PERIOD ;
+    let log = Math.log(a);
+    let res = Math.ceil(log / k );
+    return res;
+  } else {
+    return false;
+  }
 };
