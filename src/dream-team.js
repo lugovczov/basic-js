4adab87a26463ac6b5ba31f09221eef0ac0ae24a
lugovczov <vova.lugovczov@mail.ru>
2020-03-01 18:26:08 +0300

    feat: add solution for dream-team test

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let res = [];
+  
+  if ( Array.isArray(members) === true ) {
+    members = members.filter( a => a === `${a}` );
+    members = members.map( a => a = a.replace(/\s+/g, ' ').trim());
+    members.forEach( a => res.push(a[0]));
+    res = res.map( a => a.toUpperCase());
+
+    res = res.sort().join('').split();
+    return res;
+    } else {
+      return false;
+  }
+
 };
