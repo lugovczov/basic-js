95ef0215d0821a51b2e0d4be42254ed5a01c4d83
lugovczov <vova.lugovczov@mail.ru>
2020-03-04 14:20:22 +0300

    feat: add solution for task transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,43 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+    let res = [];
+    
+    if ( Array.isArray(arr) === false) {
+        return error;
+    }
+
+    for ( let i = 0; i < arr.length; i++) {
+        if (arr[arr.length-1] == '--discard-next') {
+            arr.splice(-1, 1); 
+        } else if (arr[0] == '--discard-prev') {
+            arr.splice(0, 1);  
+        } else if (arr[arr.length-1] == '--double-next') {
+            arr.splice(-1, 1); 
+        } else if(arr[0] == '--double-prev') {
+            arr.splice(0, 1); 
+        }
+    }
+    
+    for ( let i = 0; i < arr.length; i++) {
+        
+        if (arr[i] === '--discard-next' & arr[i + 2] === '--double-prev') {
+            res.push(arr[i+1]) & i++ & i++;
+        } else if (arr[i] === '--double-next' & arr[i + 2] === '--discard-prev') {
+            res.push(arr[i+1]) & i++ & i++;
+        } else if (arr[i] == '--double-next') {
+            res.push(arr[i+1]);
+        } else if(arr[i] == '--double-prev') {
+            res.push(arr[i-1]) ; 
+        } else if (arr[i] == '--discard-next') {
+            i++;
+        } else if (arr[i] == '--discard-prev') {
+            res.pop();
+        } else {
+            res.push(arr[i]);
+        }
+    }
+
+    return res;
+
+  
 };
