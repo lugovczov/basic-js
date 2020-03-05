7d6df930ce939dc9fc714a133ecf822b14c4573f
lugovczov <vova.lugovczov@mail.ru>
2020-03-05 13:51:48 +0300

    feat: add solution for simple-change task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,40 @@
 const chainMaker = {
+
+  chain: [],
+  
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.length;
+    return this;
   },
+
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if ( typeof(value) == 'undefined') {
+      this.chain.push('( )');
+    } else {
+      this.chain.push(`( ${value} )`); 
+    } 
+    return this;
   },
+
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if ( typeof( position ) == 'number' & position > 0 & position < this.chain.length) {
+      this.chain.splice(position-1, 1);
+    } else {
+      this.chain = [];
+      throw new Error();
+    }
+    return this;
   },
+
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let str = this.chain.join('~~');
+    this.chain = [];
+    return str;
   }
 };
 
